f82c6fda7e995dfc96f67b5f0631d2139a03f547
Kaustubh568 <chandelkaustubh781@gmail.com>
2025-01-16 19:33:33 +0530

    new kaam

--- src/Models/Island.jsx
+++ src/Models/Island.jsx
@@ -5,9 +5,118 @@ import { a } from '@react-spring/three';
 
 import islandScene from '../assets/3d/island.glb';
 
-const Island = (props) =>{
+export function Island({
+  isRotating,
+  setisRotating,
+  setCurrentStage,
+  currentFocusPoint,
+  ...props
+}){
   const islandRef = useRef();
-  const { nodes, materials } = useGLTF(islandScene)
+  
+  const {gl, viewport}= useThree();
+  const { nodes, materials } = useGLTF(islandScene);
+
+  const lastX = useRef(0);
+  const rotationspeed = useRef(0);
+  const dampingFactor = 0.95;
+
+  const handlepointerdown = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setisRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    lastX.current = clientX;
+
+  }
+
+  const handlepointerup = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setisRotating(false);
+  }
+
+  const handlepointermove = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (isRotating){
+      const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+      const delta = (clientX - lastX.current) /viewport.width;
+      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+      lastX.current = clientX;
+      rotationspeed.current = delta * 0.01 * Math.PI;
+    }
+  }
+  const handlekeydown = (e) => {
+    if (e.key === "ArrowLeft"){
+      if(!isRotating) setisRotating(true);
+      islandRef.current.rotation.y += -0.01 * Math.PI;
+    }else if(e.key ==="ArrowRight"){
+      if(!isRotating) setisRotating(true);
+      islandRef.current.rotation.y -= -0.01 * Math.PI;
+    }
+  }
+
+  const handlekeyup = (e) => {
+    if (e.key === "ArrowLeft" || e.key ==="ArrowRight"){
+      setisRotating(false);
+    }
+  }
+  useFrame(() => {
+    if(!isRotating){
+      rotationspeed.current *= dampingFactor;
+    }
+    if(Math.abs(rotationspeed.current)< 0.001 ){
+      rotationspeed.current = 0;
+    }else{
+      const rotation = islandRef.current.rotation.y;
+
+      const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+      
+    }
+  })
+  
+  useEffect(() =>{
+    const canvas = gl.domElement;
+    canvas.addEventListener('pointerdown', handlepointerdown);
+    canvas.addEventListener('pointerup', handlepointerup);
+    canvas.addEventListener('pointermove', handlepointermove);
+    document.addEventListener('keydown', handlekeydown);
+    document.addEventListener('keyup', handlekeyup);
+
+    return () =>{
+      canvas.removeEventListener('pointerdown', handlepointerdown);
+      canvas.removeEventListener('pointerup', handlepointerup);
+      canvas.removeEventListener('pointermove', handlepointermove);
+      document.removeEventListener('keydown', handlekeydown);
+      document.removeEventListener('keyup', handlekeyup);
+    }
+
+  },[gl, handlepointerdown, handlepointermove, handlepointerup])
+
   return (
     <a.group ref= {islandRef} {...props}>
       <mesh
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,14 @@
-import {Suspense} from 'react';
+import {Suspense, useState} from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../Components/Loader';
-import Island from '../Models/Island'
+import Island from '../Models/Island';
+import Sky from '../Models/Sky';
+import Bird from '../Models/Bird';
+import Plane from '../Models/Plane';
 
 const Home = () => {
+  const [isRotating, setisRotating] = useState(false);
+
   const adjustislandforscreen = () =>{
     let screenscale= null;
     let screenposition = [0, -6.5, -43];
@@ -17,7 +22,22 @@ const Home = () => {
     }
     return [screenscale, screenposition, rotation];
   }
+
+  const adjustPlaneforscreen = () =>{
+    let screenScale, screenPosition;
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
   
+  const [planeposition, planescale] = adjustPlaneforscreen();
   const [islandscale, islandposition, rotation] = adjustislandforscreen();
   return (
     <section className="w-full h-screen relative">
@@ -25,14 +45,27 @@ const Home = () => {
     POPUP
     </div> */}
     <Canvas 
-    className= "w-full h-screen bg-transparent"
+  className= {`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing': 'cursor-grab'}`}
     camera={{near: 0.1, far:1000}}
     >
     <Suspense fallback={<Loader />}>
+    <directionalLight position={[1, 1, 1]} intensity={1}/>
+    <ambientLight intensity={0.5} />
+    <hemisphereLight skyColor= "#b1e1ff" groundColor="#0000000" intensity={1}/>
+    <Bird />
+    <Sky />
     <Island 
       position= {islandposition}
       scale= {islandscale}
       rotation={rotation}
+      isRotating={isRotating}
+      setisRotating={setisRotating}
+    />
+    <Plane 
+       planeposition= {planeposition}
+      //scale= {planescale}
+      rotation={[0, 20, 0]}
+      isRotating={isRotating}
     />
     </Suspense>
     </Canvas>
